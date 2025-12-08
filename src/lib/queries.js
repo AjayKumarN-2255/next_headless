8d58ea2ea8d382eb663a2df0bcf1e935997756cc
AjayKumarN-2255 <kumar.ajay@midnay.com>
2025-12-08 17:35:36 +0530

    started graphql

---
+++ src/lib/queries.js
@@ -0,0 +1,21 @@
+const baseUrl = process.env.WORDPRESS_URL;
+import { gql, GraphQLClient } from "graphql-request";
+
+const client = new GraphQLClient('http://localhost:10028/headless');
+export async function getCategories(params) {
+    const query = gql`query getCategories {
+                          categories {
+                                nodes {
+                                    id
+                                    name
+                                    slug
+                                }
+                            }
+                        }`
+    const data = await client.request(query);
+    return data.categories.nodes;
+}
+
+getCategories().then((res)=>{
+    console.log(res)
+});
