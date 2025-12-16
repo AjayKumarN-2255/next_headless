b8c27eb0422f13c223e135a6f0b129dab2a7f6fe
AjayKumarN-2255 <kumar.ajay@midnay.com>
2025-12-16 12:19:16 +0530

    fetch filter wise posts completed

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,19 +5,22 @@ async function Blog(props) {
 
   const searchParams = await props.searchParams;
   const searchTerm = searchParams.search;
-  const category = searchParams.category;
+  const category = searchParams.categories;
   const before = searchParams?.before || null;
   const after = searchParams?.after || null;
 
   const { posts, pageInfo } = await getAllPosts(searchTerm, category, { before, after });
+
   const latestPostProps = {
+    posts,
+    pageInfo,
     category,
     searchTerm
-  }
+  };
 
   return (
     <section>
-      <LatestPosts />
+      <LatestPosts {...latestPostProps} />
     </section>
   )
 }
--- src/lib/queries.js
+++ src/lib/queries.js
@@ -20,6 +20,83 @@ export async function getAllPosts(searchTerm, category, params) {
 
     const hasSearchTerm = searchTerm && searchTerm.trim() !== '';
     const hasCategory = category && category.trim() !== '';
-    const isPrevious = !!params.before;
 
+    const isPrevious = !!params?.before;
+
+    //defenition
+    const varDefinition = [
+        '$perpage:Int!',
+        isPrevious ? '$before:String' : '$after:String',
+        hasSearchTerm ? '$search:String' : '',
+        hasCategory ? '$categoryName:String' : ''
+    ].filter(Boolean).join(',');
+
+    //where section
+    const whereCondition = [
+        hasSearchTerm ? 'search:$search' : '',
+        hasCategory ? 'categoryName:$categoryName' : ''
+    ].filter(Boolean);
+
+    const whereClause = whereCondition.length > 0 ?
+        `where : {${whereCondition.join(',')}}`
+        : '';
+
+    const query = gql`
+    query GetPosts(${varDefinition}) {
+    posts(
+      ${isPrevious ? 'last:$perpage' : 'first:$perpage'},
+      ${isPrevious ? 'before:$before' : 'after:$after'},
+      ${whereClause}
+    ) {
+      nodes {
+        id
+        title
+        excerpt
+        date
+        slug
+        categories {
+          nodes {
+            name
+            slug
+          }
+        }
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+     }
+   }
+   `;
+
+    const variables = {
+        perpage: 10,
+        ...(
+            isPrevious ?
+                { before: params.before }
+                :
+                { after: params.after }
+        )
+    }
+
+    if (hasSearchTerm) {
+        variables.search = searchTerm;
+    }
+
+    if (hasCategory) {
+        variables.categoryName = category;
+    }
+
+    const data = await client.request(query, variables);
+
+    return {
+        posts: data.posts.nodes,
+        pageInfo: data.posts.pageInfo,
+        ...(hasSearchTerm && { searchTerm }),
+        ...(hasCategory && { category })
+    }
 }
+
+getAllPosts();
